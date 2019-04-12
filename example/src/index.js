5a4187a47e7cfa02591cbf6b59e29684de1587d9
Jakub Musik <jakubmusik@thesoftwarehouse.pl>
2019-04-12 16:13:26 +0200

    move example to the repository, adjust links

---
+++ example/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { AppContainer } from './app/AppContainer';
+import { store } from './store/store';
+
+import './styles.css';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>,
+  rootElement,
+);
